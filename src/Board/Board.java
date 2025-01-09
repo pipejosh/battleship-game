c203aaf3c792971ccc9f016b7e6d69301f8f4e73
pipejosh <cristopher.rosas.a@hotmail.com>
2025-01-08 20:38:45 -0600

    modularize board.java

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,23 +1,23 @@
 package Board;
 
-import java.util.Arrays;
 
 public class Board 
 {
     private final int size;
     private final int[][] board; 
 
-    /*  BOARD ARRAY CAN CONTAIN 5 VALUES; 
-        0: DEFAULT / NOT DISCOVER
-        1: USER || AI SHIP 
-        2: USER || AI SHOOT
-        3: USER || AI SHIP SHOT
-        4: USER || AI SHIP DISTROYED
+    /*  BOARD ARRAY CAN CONTAIN 6 VALUES; 
+        0: empty space
+        1: ship space
+        2: shot done
+        3: shot miss
+        4: ship hit
+        5: ship destroyed
     */
-    
+
     public Board() 
     {
-        this.size = 10;
+        this.size = 10;  // Tamaño fijo para este ejemplo
         this.board = new int[size][size];
     }
     
@@ -25,7 +25,7 @@ public class Board
     {
         return size;
     }
-    
+
     public int[][] getBoard() 
     {
         return board;
@@ -33,25 +33,14 @@ public class Board
 
     public void setBoardPosition(int x, int y, int value) 
     {
-        if (!isInBounds(x, y)) return;
-
-        board[x][y] = value;
-    }
-
-    public void shot(int x, int y) 
-    {
-        if (!isInBounds(x, y)) return;
-
-        switch (board[x][y]) 
+        if (isInBounds(x, y)) 
         {
-            case 0 -> board[x][y] = 3;
-            case 1 -> board[x][y] = 4;
+            board[x][y] = value;
         }
     }
 
-    public boolean isInBounds(int x, int y)
+    public boolean isInBounds(int x, int y) 
     {
         return x >= 0 && x < size && y >= 0 && y < size;
     }
-
 }
