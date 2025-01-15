72cb0aebe9cff45f403567d91da0e9ff683ee841
Lizziee44 <elizabeths10@live.wsd1.org>
2025-01-15 14:21:26 -0600

    barebones of music

--- src/MusicScript/PlayMusic.java
+++ src/MusicScript/PlayMusic.java
@@ -1,5 +1,20 @@
 package MusicScript;
 
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+
+
 public class PlayMusic {
-    
+    Long currentFrame;
+    Clip clip;
+
+    String status;
+    AudioInputStream audioInputStream;
+    static String play;
+
+    public void musicPlayer(){
+        URL musicUrl = getClass().getResource(play);
+
+    }
 }
