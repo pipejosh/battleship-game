0b435a1c7c90f1acc34cfdec95cbfe73e7cf616b
pipejosh <cristopher.rosas.a@hotmail.com>
2025-01-16 13:57:26 -0600

    change titles of the winows on board logic

--- src/Board/BoardLogic.java
+++ src/Board/BoardLogic.java
@@ -23,22 +23,40 @@ public class BoardLogic
         aiShipPlace = new DisplayShipPlace(aiBoard);
         aiShipPlace.setOnShipsPlacedCallback(this::aiOnShipsPlaced);
 
+        changeTitles();
     }
 
     public void changeTitles() 
     {
+        shipPlace.setTitle("TEST");
+        aiShipPlace.setTitle("OTHER TEST");
+    }
+
+    public void changeOtherTitle()
+    {
+        viewShips.setTitle("TEST!!!!");
+        attackShips.setTitle("TEST!!!!!!!!!!");
+    }
+
+    public void changeOtherOtherTitle()
+    {
+        aiViewShips.setTitle("CHEST!!!!");
+        aiAttackShips.setTitle("CHEST!!!!!!!!!!");
     }
 
     public void onShipsPlaced()
     {
         viewShips = new DisplayViewGrid(playerBoard);
         attackShips = new DisplayAttackGrid(aiBoard, null);
+        changeOtherTitle();
     }
     
     public void aiOnShipsPlaced()
     {
         aiViewShips = new DisplayViewGrid(aiBoard);
         aiAttackShips = new DisplayAttackGrid(playerBoard, null);
+        changeOtherOtherTitle();
+
     }
 
     public static void main(String[] args) 
