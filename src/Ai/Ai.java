d55439e73b93e3ba73d6b04f76e0517d27920ce3
pipejosh <cristopher.rosas.a@hotmail.com>
2025-01-18 12:15:43 -0600

    add random ship placement

--- src/Ai/Ai.java
+++ src/Ai/Ai.java
@@ -1,9 +1,47 @@
 package Ai;
 
 import Board.*;
+import Ship.PlaceShips;
+
 import java.util.*;
 
 public class Ai 
 {
-    public void determinePorcentarge()
+    private Board board = new Board();
+    private PlaceShips shipPlacer = new PlaceShips(board);
+    
+    private Random random = new Random();
+
+    private int randomX = 0;
+    private int randomY = 0;
+    private boolean randomHorizontal = false;
+
+    // public Ai(Board board)
+    // {
+    //     this.board = board;
+    // }
+
+    public void setShips()
+    {
+        while (shipPlacer.getShipsLeft() > 0)
+        {
+            randomHorizontal = random.nextBoolean();
+            randomX = random.nextInt(board.getSize());
+            randomY = random.nextInt(board.getSize());
+            shipPlacer.setIsHoriazontal(randomHorizontal);
+            shipPlacer.placeShips(randomX, randomY);
+        }
+
+        System.out.println("OUT OF LOOP");
+
+        board.printBoard();
+    }
+
+    public static void main(String[] args)
+    {
+        Ai testAi = new Ai(); 
+
+        testAi.board.printBoard();
+        testAi.setShips();
+    }
 }
