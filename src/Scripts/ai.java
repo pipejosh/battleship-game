8d7b8b40397154a2bd2b7357061968e15d381515
Yarik228666 <yaroslavb@live.wsd1.org>
2025-01-21 14:14:33 -0600

    work on stupid ai

--- src/Scripts/ai.java
+++ src/Scripts/ai.java
@@ -19,13 +19,17 @@ public class ai {
     //так же если оно делает выстрел и попадает, дальше стреляет в правильном направлении и корабль не уничтожается (типо там конец) то оно возвращается на прошлую точку и стреляет-
     //- в противоположном направлении и уничтожает корабль
     // оно не должно выходить за рамки полигона.
-/* 
+ 
     Random random = new Random();
     
     int Xplace = 0;
     int Yplace = 0;
     int Xhit = 0;
     int Yhit = 0;
+    int Rotation = 0;
+    int hitPointX = 0;
+    int hitPointY = 0;
+    Boolean shipDestroyed = false;
     Boolean randomVertical = false;
 
     public Board aiBoard;
@@ -36,6 +40,12 @@ public class ai {
         this.aiBoard = board;
         this.shipPlacer = shipPlace;
     } //настраивает
+
+    public ai()
+    {
+        this.aiBoard = new Board();
+        this.shipPlacer = new PlaceShips(aiBoard);
+    }
 
     public void setShip()
     {
@@ -51,22 +61,142 @@ public class ai {
     }
 
     public void setHit()
-    {
-        Xhit = random.nextInt(aiBoard.getSize());
-        Yhit = random.nextInt(aiBoard.getSize());
+    {   
+        //хочу сделать точку возрата то бишь Yhit и Xhit и после них hitPointX и hitPointY с помощью которых ии будет стрелять точно в цель
+        //к примеру ии попадает в корабль и ставить контрольные точки Yhit и Xhit, после этого он выбирает рандомную точку для выстрела (главное не за границы) и стреляет - 
+        // - оставляя координаты нового выстрела с помощью hitPoint-ов
+        //если мимо то он возвращается на контрольные точки и стреляет в другую сторону и повторяет историю с коордами
+        //если он стрелял успешно, но вдруг корабль оборвался по пути и не уничтожился то он опять возвращается на контрольные точки и стреляет в противоположную сторону
 
+        Xhit = random.nextInt(10);
+        Yhit = random.nextInt(10);
+        
         aiBoard.setHit(Xhit, Yhit);
-        //if(aiBoard)
+
+        if (aiBoard.getBoardPosition(Xhit, Yhit).equals("HIT"))
+        
+        {
+            System.out.println("123");
+
+            shipDestroyed = false;
+            while(!shipDestroyed){
+
+                if (aiBoard.getBoardPosition(Xhit, Yhit).equals("DESTROYED")){
+                    
+                }else{
+                    Rotation = random.nextInt(4);
+                
+
+            if (Rotation == 0){
+                if(Xhit +1 == 10){
+
+                }else{
+                    hitPointX = Xhit + 1;
+                aiBoard.setHit(hitPointX, Yhit);
+                    
+                if (aiBoard.getBoardPosition(hitPointX, Yhit).equals("HIT")){
+
+                    if (aiBoard.getBoardPosition(hitPointX, Yhit).equals("DESTROYED")){
+                    shipDestroyed = true;
+                   }else{
+                    hitPointX = hitPointX + 1;
+                    aiBoard.setHit(hitPointX, Yhit);
+
+                        if (aiBoard.getBoardPosition(hitPointX, Yhit).equals("DESTROYED")){
+                        shipDestroyed = true;
+                        }else if(aiBoard.getBoardPosition(hitPointX, Yhit).equals("HIT")){
+                            hitPointX = hitPointX + 1;
+                            aiBoard.setHit(hitPointX, Yhit);
+
+                        }else{
+                            hitPointX = Xhit - 1;
+                            aiBoard.setHit(hitPointX, Yhit);
+                            if (aiBoard.getBoardPosition(hitPointX, Yhit).equals("DESTROYED")){
+                                shipDestroyed = true;
+                            }else{
+                                hitPointX = hitPointX - 1;
+                                aiBoard.setHit(hitPointX, Yhit);
+
+                            }
+                        }
+                   }
+
+                }else{
+
+                }
+
+                
+
+                }
+
+
+            } else if (Rotation == 1){
+                if(Xhit -1 == -1){
+
+                }else{
+                    hitPointX = Xhit - 1;
+                aiBoard.setHit(hitPointX, Yhit);
+                }
+
+
+            }else if (Rotation == 2){
+                if(Yhit + 1 == 10){
+
+                }else{
+                    hitPointY = Yhit + 1;
+                aiBoard.setHit(Xhit, hitPointY);
+                }
+
+
+            }else if (Rotation == 3){
+                if(Yhit -1 == -1){
+
+                }else{
+                    hitPointY = Yhit - 1;
+                aiBoard.setHit(Xhit, hitPointY);
+                }
+
+
+            }else{
+                
+            }
+
+
+          }
+
+                
+
+       }
+            
 
         
     }
-*/
+
+
+   }
+
+   public void printBoard()
+   {
+    aiBoard.printBoard();
+   }
+
+   public static void main(String[] args) 
+   {
+        ai test = new ai();
+
+        test.printBoard();
+        test.setShip();
+        test.printBoard();
+        test.setHit();
+        test.printBoard();
+    }
+        
+
+ }
 
 
 
-    
 
 
 
-    //оно должно отдавать отчет о 
-}
+
