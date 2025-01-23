883aba2fa5d1fd9a3de65f0b5a53779a9cd4d521
Yarik228666 <yaroslavb@live.wsd1.org>
2025-01-23 13:36:05 -0600

    comment main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,19 +5,21 @@ import Board.*;
 
 public class Main 
 {
+    //submites BoardLogic
     public BoardLogic test;
-
+    //creates BoardLogic
     public Main()
     {
         this.test = new BoardLogic();
     }
-
+    //creates a function that starts a game again
     public void playAgain()
     {
         test.startsActualGame();
     }
     public static void main(String[] args) 
     {
+        //approves main
         Main main = new Main();
         main.playAgain();
     }    
