f9ebd347809947acfa9758001450d27d6b1895c9
pipejosh <cristopher.rosas.a@hotmail.com>
2025-01-23 14:07:07 -0600

    make ai more smarter

--- src/Ai/AdvanceAi.java
+++ src/Ai/AdvanceAi.java
@@ -124,7 +124,7 @@ public class AdvanceAi
             hitChange += 10;
         }
 
-        int randomHitChange = random.nextInt(hitChange) + 5;
+        int randomHitChange = random.nextInt(hitChange) + 2;
 
         if (randomHitChange <= magicNumber)
         {
