530d5a6f09f6a864c019e5f78937f126ad2493d7
Lizziee44 <elizabeths10@live.wsd1.org>
2025-01-23 13:31:54 -0600

    Ship commented

--- src/Ship/Ship.java
+++ src/Ship/Ship.java
@@ -16,20 +16,20 @@ public class Ship {
         SUBMARINE(3), 
         DESTROYER(2),
         BOAT(1);
-
+//the five types of ships to be placed
         private final int size;
 
         ShipType(int size) 
         {
             this.size = size;
         }
-
+//gets the ship size 
         public int getSize() 
         {
             return size;
         }
     }
-
+//ship constructor
     public Ship(ShipType type) 
     {
         this.shipType = type;
@@ -37,17 +37,17 @@ public class Ship {
         this.shipHealth = shipSize; 
         this.isDestroyed = false;
     }
-
+//Gets the ship size 
     public int getShipSize() 
     {
         return shipSize;
     }
-
+//Gets the ship health
     public int getShipHealth() 
     {
         return shipHealth;
     }
-
+//Checks if the ship is destroyed and changes it to true
     public boolean getIsDestroyed() 
     {
         if (shipHealth == 0)
@@ -57,12 +57,12 @@ public class Ship {
 
         return isDestroyed;
     }
-
+//gets the ship type
     public ShipType getShipType() 
     {
         return shipType;
     }
-
+//Gets the ship name from the ship type
     public String getShipName()
     {
         return shipType.toString();
